17a914160bc016e3a19686fd11c5ac9bf94b7df9
Estefany Hernández Arce <32408425+Estef-Her@users.noreply.github.com>
2019-04-20 14:10:31 -0600

    SEC

---
+++ ROP/ROP/CalculadoraIncentivoFiscal.vb
@@ -0,0 +1,144 @@
+﻿Public Class CalculadoraIncentivoFiscal
+
+    'Definicion de atributos'
+    Private porcentajeLimiteSalario As Double
+    Private primerTramoRenta As Double
+    Private segundoTramoRenta As Double
+    Private tercerTramoRenta As Double
+    Private primerLimiteRenta As Double
+    Private segundoLimiteRenta As Double
+    Private primerPorcentajeCargaSocial As Double
+    Private segundoPorcentajeCargaSocial As Double
+
+    Public Sub New()
+        Me.porcentajeLimiteSalario = 10 / 100
+        Me.primerTramoRenta = 15 / 100
+        Me.segundoTramoRenta = 10 / 100
+        Me.tercerTramoRenta = 0
+        Me.primerLimiteRenta = 799000.0
+        Me.segundoLimiteRenta = 1199000.0
+        Me.primerPorcentajeCargaSocial = 5.5 / 100
+        Me.segundoPorcentajeCargaSocial = 3.34 / 100
+    End Sub
+
+    'Definicion de metodos'
+    Public Sub cambiarPorcentajeLimiteSalario(ByVal porcentaje As Double)
+        porcentajeLimiteSalario = porcentaje / 100
+    End Sub
+
+    Public Sub cambiarPrimerTramoRenta(ByVal tramo As Double)
+        primerTramoRenta = tramo / 100
+    End Sub
+
+    Public Sub cambiarSegundoTramoRenta(ByVal tramo As Double)
+        segundoTramoRenta = tramo / 100
+    End Sub
+
+    Public Sub cambiarTercerTramoRenta(ByVal tramo As Double)
+        tercerTramoRenta = tramo / 100
+    End Sub
+
+    Public Sub cambiarPrimerLimiteRenta(ByVal limite As Double)
+        primerLimiteRenta = limite
+    End Sub
+
+    Public Sub cambiarSegundoLimiteRenta(ByVal limite As Double)
+        segundoLimiteRenta = limite
+    End Sub
+
+    Public Sub cambiarPrimerPorcentajeCargaSocial(ByVal monto As Double)
+        primerPorcentajeCargaSocial = monto / 100
+    End Sub
+
+    Public Sub cambiarSegundoPorcentajeCargaSocial(ByVal monto As Double)
+        segundoPorcentajeCargaSocial = monto / 100
+    End Sub
+
+    Public Function obtenerPorcentajeLimiteSalario() As Double
+        Return porcentajeLimiteSalario
+    End Function
+
+    Public Function obtenerPrimerTramoRenta() As Double
+        Return primerTramoRenta
+    End Function
+
+    Public Function obtenerSegundoTramoRenta() As Double
+        Return segundoTramoRenta
+    End Function
+
+    Public Function obtenerTercerTramoRenta() As Double
+        Return tercerTramoRenta
+    End Function
+
+    Public Function obtenerPrimerLimiteRenta() As Double
+        Return primerLimiteRenta
+    End Function
+
+    Public Function obtenerSegundoLimiteRenta() As Double
+        Return segundoLimiteRenta
+    End Function
+
+    Public Function obtenerPrimerPorcentajeCargaSocial() As Double
+        Return primerPorcentajeCargaSocial
+    End Function
+
+    Public Function obtenerSegundoPorcentajeCargaSocial() As Double
+        Return segundoPorcentajeCargaSocial
+    End Function
+
+    Public Function calculoMontoMaximoPorExonerar(ByVal usuario As Usuario) As Double
+        Return (usuario.obtenerSalarioMensual() * obtenerPorcentajeLimiteSalario())
+    End Function
+
+    Public Function calculoSalarioParaImpuestos(ByVal usuario As Usuario) As Double
+        Dim salario As Double
+
+        If (usuario.obtenerSalarioMensual() * obtenerPorcentajeLimiteSalario()) > usuario.obtenerMontoAporte() Then
+            salario = usuario.obtenerSalarioMensual() - usuario.obtenerMontoAporte()
+        Else
+            salario = usuario.obtenerSalarioMensual() - (usuario.obtenerSalarioMensual() * obtenerPorcentajeLimiteSalario())
+        End If
+
+        Return salario
+    End Function
+
+    Public Function calculoMontoImpuestoRenta(ByVal valor As Double) As Double
+        Dim primerValor As Double = 0.00
+        Dim segundoValor As Double = 0.00
+        Dim tercerValor As Double = 0.00
+        Dim monto As Double
+
+        If valor <= obtenerPrimerLimiteRenta() Then
+            primerValor = valor * obtenerTercerTramoRenta()
+        End If
+
+        If valor > obtenerPrimerLimiteRenta() And valor <= obtenerSegundoLimiteRenta() Then
+            segundoValor = valor * obtenerSegundoTramoRenta()
+        End If
+
+        If valor > obtenerSegundoLimiteRenta() Then
+            tercerValor = valor * obtenerPrimerTramoRenta()
+        End If
+
+        monto = primerValor + segundoValor + tercerValor
+
+        Return monto
+    End Function
+
+    Public Function calculoMontoCargasSociales(ByVal valor As Double) As Double
+        Return (valor * obtenerPrimerPorcentajeCargaSocial()) + (valor * obtenerSegundoPorcentajeCargaSocial())
+    End Function
+
+    Public Function sumaTotalImpuestos(ByVal primerValor As Double, ByVal segundoValor As Double) As Double
+        Return primerValor + segundoValor
+    End Function
+
+    Public Function diferenciaImpuestos(ByVal primerValor As Double, ByVal segundoValor As Double) As Double
+        Return primerValor - segundoValor
+    End Function
+
+    Public Function calculoProyeccion(ByVal valor As Double, ByVal usuario As Usuario) As Double
+        Return usuario.obtenerMontoAporte() - valor
+    End Function
+
+End Class
---
+++ ROP/ROP/validation.js
@@ -0,0 +1,196 @@
+﻿$(function () {
+
+    $("#form1").validate({
+        rules: {
+            campoTextoCapital: { number: true },
+            campoTextoNacimiento: { number: true },
+            campoTextoTasa: { number: true }
+        },
+        messages: {
+            'campoTextoCapital': {
+                number: jQuery('.input_txtCapital').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || 44 < tecla.charCode && tecla.charCode < 48 || tecla.charCode > 57) return false;
+                })
+            },
+            'campoTextoTasa': {
+                number: jQuery('.input_txtTasa').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 46 || tecla.charCode > 57 || tecla.charCode==47) return false;
+                })
+            },
+        }
+    });
+});
+
+$(function () {
+    $("#form1").validate({
+        rules: {
+            rendimiento1: { required: true, number: true },
+            rendimiento2: { required: true, number: true },
+            rendimiento3: { required: true, number: true },
+            rendimiento4: { required: true, number: true },
+            rendimiento5: { required: true, number: true },
+            rendimiento6: { required: true, number: true },
+            rendimiento7: { required: true, number: true },
+            rendimiento8: { required: true, number: true },
+            rendimiento9: { required: true, number: true },
+            rendimiento10: { required: true, number: true },
+            rendimiento11: { required: true, number: true },
+            rendimiento12: { required: true, number: true },
+            comision1: { required: true, number: true },
+            comision2: { required: true, number: true },
+            comision3: { required: true, number: true },
+            comision4: { required: true, number: true },
+            comision5: { required: true, number: true },
+            comision6: { required: true, number: true },
+            comision7: { required: true, number: true },
+            comision8: { required: true, number: true },
+            comision9: { required: true, number: true },
+            comision10: { required: true, number: true },
+            comision11: { required: true, number: true },
+            comision12: { required: true, number: true }
+        },
+        messages: {
+            'rendimiento1': {
+                number: jQuery('.rendimiento1').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'rendimiento2': {
+                number: jQuery('.rendimiento2').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'rendimiento3': {
+                number: jQuery('.rendimiento3').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'rendimiento4': {
+                number: jQuery('.rendimiento4').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'rendimiento5': {
+                number: jQuery('.rendimiento5').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'rendimiento6': {
+                number: jQuery('.rendimiento6').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'rendimiento7': {
+                number: jQuery('.rendimiento7').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'rendimiento8': {
+                number: jQuery('.rendimiento8').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'rendimiento9': {
+                number: jQuery('.rendimiento9').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'rendimiento10': {
+                number: jQuery('.rendimiento10').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'rendimiento11': {
+                number: jQuery('.rendimiento11').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'rendimiento12': {
+                number: jQuery('.rendimiento12').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'comision1': {
+                number: jQuery('.comision1').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'comision2': {
+                number: jQuery('.comision2').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'comision3': {
+                number: jQuery('.comision3').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'comision4': {
+                number: jQuery('.comision4').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'comision5': {
+                number: jQuery('.comision5').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'comision6': {
+                number: jQuery('.comision6').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'comision7': {
+                number: jQuery('.comision7').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'comision8': {
+                number: jQuery('.comision8').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'comision9': {
+                number: jQuery('.comision9').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'comision10': {
+                number: jQuery('.comision10').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'comision11': {
+                number: jQuery('.comision11').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+            'comision12': {
+                number: jQuery('.comision12').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            }
+        }
+    });
+});
+
+$(function () {
+    $("#form1").validate({
+        rules: {
+            campoTextoSalario: { number: true },
+            campoTextoAporte: { number: true }
+        },
+        messages: {
+            'campoTextoSalario': {
+                number: jQuery('.campoTextoSalario').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 44 || 44 < tecla.charCode && tecla.charCode < 48 || tecla.charCode > 57) return false;
+                })
+            },
+            'campoTextoAporte': {
+                number: jQuery('.campoTextoAporte').keypress(function (tecla) {
+                    if (31 < tecla.charCode && tecla.charCode < 46 || tecla.charCode > 57 || tecla.charCode == 47) return false;
+                })
+            },
+        }
+    });
+});
